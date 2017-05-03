f2c1f5cc12ce94aba30a39b1d1079b6eafaf90c2
roman <ravoro@gmail.com>
2017-05-03 20:40:30 +0100

    use flask cli command to run dev server

--- run.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-# from flask_debugtoolbar import DebugToolbarExtension
-
-app = create_app("config.DevConfig")
-
-# initialize debugtoolbar
-# DebugToolbarExtension(app)
-
-app.run()
--- scripts/run_dev.sh
+++ scripts/run_dev.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+# Run local flask dev server
 
-cd $sites/virtual-runner
+if [ ! -d venv ]; then
+    echo 'script needs to be run from project base dir'
+    exit
+fi
+
 source venv/bin/activate
-python run.py
+export FLASK_CONFIG='config.DevConfig'
+export FLASK_APP='app:app_from_envvar'
+export FLASK_DEBUG=1
+flask run
